69f360b0230d6454d517197af548ea55fd8e7509
juniorferreira23 <junior.ferreira@outlook.com>
2024-08-27 20:22:30 -0300

    FEAT: finalizando tela de cadastro

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,41 +3,29 @@ import CredentialsProvider from "next-auth/providers/credentials"
 
 
 const handler = NextAuth({
-    pages: {
-        signIn: "/login"
-    },
-    providers: [
-        CredentialsProvider({
-          name: 'Credentials',
-          credentials: {
-            email: { label: "email", type: "email", placeholder: "E-mail" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            // console.log(credentials)
-            if(!credentials) return null
-
-            if(credentials.email === "junior.ferreira@outlook.com" && credentials.password === "123"){
-                return {
-                    id: "1",
-                    name: "Junior",
-                    email: "junior.ferreira@outlook.com"
-                }
-            }
-            // const res = await fetch("/your/endpoint", {
-            //   method: 'POST',
-            //   body: JSON.stringify(credentials),
-            //   headers: { "Content-Type": "application/json" }
-            // })
-            // const user = await res.json()
-      
-            // if (res.ok && user) {
-            //   return user
-            // }
-            // return null
+  pages: {
+      signIn: "/login"
+  },
+  providers: [
+      CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          email: { label: "email", type: "email", placeholder: "E-mail" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials) {
+          if(credentials.email === "junior.ferreira@outlook.com" && credentials.password === "123"){
+              return {
+                  id: "1",
+                  name: "Junior",
+                  email: "junior.ferreira@outlook.com"
+              }
+          }else{
+            throw new Error("Credential invalid")
           }
-        })
-      ]
+        }
+      })
+  ]
 })
 
 export { handler as GET, handler as POST }
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,11 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import FormSignIn from "../components/templates/FormSignIn";
 
 export default function Login() {
+  const router = useRouter()
+
     return (
       <main className="bg-blue-900 px-5">
         <div className="container max-auto">
